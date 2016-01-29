178a9ae1de574f7757a12edb80b77f433239c115
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-30 00:56:53 +0100

    First iteration on pressure plates

---
+++ GGJ16/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+    public bool pressed = false;
+
+    Animator animator;
+    
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	   animator.SetBool("Pressed", pressed);
+	}
+
+    void OnTriggerEnter() {
+        pressed = true;
+    }
+
+    void OnTriggerExit() {
+        pressed = false;
+    }
+}
