441b8b6bde602bdec4333df6aa384c4f3cf3dda6
Thomas Stassin <thomas.stassin@gsmproductions.com>
2016-01-30 01:13:04 +0100

    grab: Ok

--- GGJ16/Assets/Scripts/Grab.cs
+++ GGJ16/Assets/Scripts/Grab.cs
@@ -1,15 +1,54 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Grab : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public const string GRABABLE_TAG = "grabable";
+public Transform object_grabable = null;
+
+    void Update() {
+        if(Input.GetMouseButton(0)) {
+            if(object_grabable != null) {
+                grab();
+            }
+        } 
+        else {
+            if(object_grabable != null) {
+                unGrab();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == GRABABLE_TAG) {
+            object_grabable = other.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.transform == object_grabable) {
+            object_grabable = null;
+        }
+    }
+
+    private void grab() {
+        try{
+            object_grabable.GetComponent<Rigidbody>().useGravity = false;
+            object_grabable.GetComponent<Rigidbody>().velocity = Vector3.zero;
+             object_grabable.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; 
+        }
+        catch{
+        }
+        object_grabable.parent = transform;
+        object_grabable.position = transform.position;
+    }
+
+    private void unGrab() {
+        try{
+            object_grabable.GetComponent<Rigidbody>().useGravity = true;
+        }
+        catch{
+        }
+        object_grabable.parent = null;
+    }
 }
