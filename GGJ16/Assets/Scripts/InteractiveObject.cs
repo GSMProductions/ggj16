a0055cb215cb36cb5d9d4ff3473a58a2c93e3105
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-30 08:04:49 +0100

    Lamp rotating

---
+++ GGJ16/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : MonoBehaviour {
+
+    public bool active;
+
+    void OnTriggerEnter() {
+
+    }
+
+    void OnTriggerExit() {
+        
+    }
+}
---
+++ GGJ16/Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseRotate : MonoBehaviour {
+
+    public float speed;
+    public float friction;
+
+    public float lerpSpeed;
+
+    private float xDeg;
+    private float yDeg;
+
+    private Quaternion fromRotation;
+    private Quaternion toRotation;
+
+    void Update () {
+        if(Input.GetMouseButton(0)) {
+            xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+            yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+        }
+        fromRotation = transform.rotation;
+        toRotation = Quaternion.Euler(yDeg,xDeg,0);
+        transform.rotation = Quaternion.Lerp(fromRotation,toRotation,Time.deltaTime  * lerpSpeed);
+    }
+}
