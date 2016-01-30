fc34631bed8cb61db0137b1635fd8cfde70f399b
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-30 11:42:24 +0100

    Added box model for the room and improved laser beam

---
+++ GGJ16/Assets/Scripts/LightRay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRay : MonoBehaviour {
+     
+    private RaycastHit hit;
+    private Ray ray;
+    public LineRenderer line;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Ray ray = new Ray(transform.position, transform.forward);
+        line.SetPosition(0, ray.origin);
+        
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            line.SetPosition(1, hit.point);
+            if (hit.collider.gameObject.tag == "grabable") {
+                // We hit the sphere
+            }
+        }
+
+	}
+}
