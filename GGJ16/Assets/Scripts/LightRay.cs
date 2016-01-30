f74b0a853ee5b78838292b2eaeb7068d2dbaeb7d
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-30 14:01:04 +0100

    Added the light bounce behaviour

--- GGJ16/Assets/Scripts/LightRay.cs
+++ GGJ16/Assets/Scripts/LightRay.cs
@@ -7,9 +7,15 @@ public class LightRay : MonoBehaviour {
     private Ray ray;
     public LineRenderer line;
 
+    public LineRenderer line_from_sphere;
+
+    public Transform ray_target;
+
+    public Transform[] targets;
+
     // Use this for initialization
     void Start () {
-    
+        line_from_sphere.enabled = false;
     }
     
     // Update is called once per frame
@@ -17,12 +23,17 @@ public class LightRay : MonoBehaviour {
         Ray ray = new Ray(transform.position, transform.forward);
         line.SetPosition(0, ray.origin);
         
-        if(Physics.Raycast(ray, out hit, 100))
-        {
+        if(Physics.Raycast(ray, out hit, 100)) {
             line.SetPosition(1, hit.point);
             if (hit.collider.gameObject.tag == "grabable") {
-                // We hit the sphere
-            }
+                if (ray_target) {
+                    line_from_sphere.enabled = true;   
+                    line_from_sphere.SetPosition(0, hit.collider.transform.position);
+                    line_from_sphere.SetPosition(1, ray_target.position);
+                }
+            } else {
+                line_from_sphere.enabled = false;
+            }              
         }
 
 	}
