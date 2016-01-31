a226797a7e46a0a22ffe2b65722748d0c001fe43
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-31 14:45:17 +0100

    Updated mixer

---
+++ GGJ16/Assets/Scripts/Ending.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class Ending : MonoBehaviour {
+
+    public AudioMixerSnapshot bgm;
+    public GameObject all;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            bgm.TransitionTo(2.0f);
+            Destroy(all); 
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
