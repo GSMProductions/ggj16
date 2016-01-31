32d17d83853f30c4c673f948b2c09d54d2860d21
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-01-31 15:07:52 +0100

    Add credits

--- GGJ16/Assets/Scripts/Ending.cs
+++ GGJ16/Assets/Scripts/Ending.cs
@@ -6,21 +6,30 @@ public class Ending : MonoBehaviour {
 
     public AudioMixerSnapshot bgm;
     public GameObject all;
+    public GameObject credits;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+    IEnumerator DelayExit(float delay) {
+        yield return new WaitForSeconds(delay);
+        Application.Quit();
+    }
+    
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
             bgm.TransitionTo(2.0f);
             Destroy(all); 
+            credits.SetActive(true);
+            StartCoroutine(DelayExit(30f));
         }
     }
+    
+    // Update is called once per frame
+    void Update () {
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
 }
