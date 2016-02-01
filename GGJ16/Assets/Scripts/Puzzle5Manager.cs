7d4cb30a7d5cbd20d7f3db14f0eb309156e991eb
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2016-02-01 13:05:46 +0100

    Final changes from the jam

---
+++ GGJ16/Assets/Scripts/Puzzle5Manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle5Manager : MonoBehaviour {
+
+    public Stuck stuck;
+    public TransitionManager manager;
+    public bool complete = false;
+
+	// Use this for initialization
+	void Start () {
+	   manager = GameObject.Find("TransitionManager").GetComponent<TransitionManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if ((manager.current_puzzle == 5) && (stuck.object_in != null) && (!complete)) {
+            print("Level 5 done");
+            manager.LevelComplete(5);
+            complete = true;
+        }
+	}
+}
