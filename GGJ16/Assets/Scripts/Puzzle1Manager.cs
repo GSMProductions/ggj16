108fded957c582dc587d2719930f780507492ff7
Bastien Gorissen <b.gorissen@voxteneo.com>
2016-02-04 17:14:44 +0100

    New animator for Up Down

--- GGJ16/Assets/Scripts/Puzzle1Manager.cs
+++ GGJ16/Assets/Scripts/Puzzle1Manager.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class Puzzle1Manager : MonoBehaviour {
 
     public TransitionManager manager;
+    bool started = false;
     public bool complete = false;
+
+    public UnityEvent on_started;
 
 	// Use this for initialization
 	void Start () {
 	   manager = GameObject.Find("TransitionManager").GetComponent<TransitionManager>();
 	}
+
+    public void BallRemovedFromBack() {
+        if (manager.current_puzzle == 1 && !started) {
+            started = true;
+            on_started.Invoke();
+        }
+    }
 
     public void CentralPedestalObjectIn(GameObject obj) {
         if (manager.current_puzzle != 1) {
---
+++ GGJ16/Assets/Scripts/UpDownMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+public class UpDownMover : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	   animator = GetComponent<Animator>();
+	}
+	
+    public void SetParameterUp(bool val) {
+        animator.SetBool("Up", val);
+    }
+}
